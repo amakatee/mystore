1dc6402f48e703c98fb41323dd514111339eb959
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-05-24 02:08:10 +0300

    nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,9 +6,22 @@ const Navbar = () => {
             <div className='nabvar-content'>
                 
               
-                <div>burg</div>
-                <h1 id='logo'>Katya's store</h1>
-                <div>cors</div>
+                <div className='flex flex-col justify-between gap-1'>
+                    <span className='w-[28px] h-[1.4px] bg-[#fff]'></span>
+                    <span className='w-[28px] h-[1.4px] bg-[#fff]'></span>
+                {/* <svg viewBox="0 0 100 100" width="33" height="20"fill='#fff' >
+                    <rect width="400" height="8"  ></rect>
+                    <rect y="30" width="400" height="8"></rect>
+                   <rect y="30" width="400" height="8"></rect>
+                </svg> */}
+                </div>
+                <h1 id='logo' className='custom-font'>Katya's store</h1>
+                
+                <div className='flex items-center gap-.6'>
+                <svg id="bag" class="icon-bag" xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24"><polygon points="18.78 20.82 5.22 20.82 5.22 9.39 18.78 9.39 18.78 20.82" fill="none" stroke="currentColor" stroke-miterlimit="10"></polygon><path d="M8.79,11.35V6.89a3.21,3.21,0,0,1,6.42,0v4.46" fill="none" stroke="currentColor" stroke-miterlimit="10" vector-effect="non-scaling-stroke"></path></svg>
+                <span className='text-xs'>(0)</span>
+                </div>
+             
              </div>
       
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const Home: NextPage = () => {
       <Navbar />
 
       <main className="w-[100vw] min-h-[100vh]">
+       <div className='relative'>  
         <div className='video-container'>
           <video loop muted autoPlay>
             <source
@@ -17,7 +18,31 @@ const Home: NextPage = () => {
             </source>
           </video>
         </div>
-        <div className='w-[100vw] min-h-[100vh]'></div>
+        <div className='absolute bottom-20 left-10  flex flex-col items-center gap-.5 ' >
+          <h1 className='text-white custom-font text-[2rem]'>Shop Now</h1>
+         <div className='white-arrow w-[50px]'>
+          <svg viewBox="0 0 44 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill='#fff' fill-rule="evenodd" clip-rule="evenodd" d="M0 5.5H43V6.5H0V5.5Z"></path>
+          <path fill='#fff' d="M43.9447 5.90506L35.167 0.914272C35.051 0.848274 34.9381 1.00447 35.0394 1.09088L40.6984 5.91727C40.75 5.96124 40.7492 6.04024 40.6968 6.08323L35.0655 10.7047C34.9624 10.7893 35.0727 10.9477 35.19 10.8835L43.9431 6.09454C44.0182 6.05345 44.0191 5.94737 43.9447 5.90506Z"></path>
+        </svg>
+        </div> 
+           
+        </div>
+        </div>
+        <div className='w-[100vw] min-h-[100vh] grid content-center'>
+          <div className='braclet-video-cont' >
+          <video loop muted autoPlay className='video'>
+            <source
+            src='/bracelet.mp4'
+            type='video/mp4'
+            >
+            </source>
+          </video>
+          </div>
+          <h1>accessories</h1>
+         
+        </div>
+        
        
       </main>
 
