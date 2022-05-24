ef2b413be6258df8e3c60ac1c9079823cf87d489
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-05-24 18:32:21 +0300

    commit changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,25 +5,9 @@ import SecondSection from './../components/main/SecondSection'
 
 
 const Home: NextPage = () => {
-  // var mainVideo = function(props){
-  //   return (
-  //     <div dangerouslySetInnerHTML={{__html:`
-  //     <video loop={true} muted={true} autoPlay={true} playsInline controls={false} className="video-main">
-  //     <source
-  //     src='/backvideo.mp4'
-  //     type='video/mp4'
-  //     >
-  //     </source>
-  //   </video>
 
-  //     `}}>
-
-  //     </div>
-
-  //   )
-  // }
   return (
-    <div className="">
+    <div>
      
 
       <main className="w-[100vw] min-h-[100vh]">
