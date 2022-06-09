9e99bfaf6f92fb6dfb3207b642c772b2abae81ee
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-06-09 16:26:57 +0300

    update meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,7 @@ import {StateContext} from './../context/StateContext'
 function MyApp({ Component, pageProps }: AppProps) {
   return <> 
   <Head>
-    <meta name="viewport" content="viewport-fit=cover" />
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" /> 
   </Head>
   <StateContext>
   <Layout>
